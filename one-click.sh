19d9402e2175b37e42bc50f3ca1b2a388e6e10d1
Sonata7 <63850356+Sonata7@users.noreply.github.com>
2020-04-17 11:07:32 +0000

    Create one-click.sh

---
+++ one-click.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo $Green  
+ echo
+
+figlet -f big Sønata
+echo "This script is only for beginner"
+
+apt-get update -y                      #update in progress
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+
+echo "wait a moment"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+
+echo "min saw nga pay pa lar"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo "Sa tar naw"
+
+exit
